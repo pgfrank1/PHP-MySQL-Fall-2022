562e87f8318a07fd300f232dcfaef495b5a7243c
pgfrank1 <patrickfrank1995@gmail.com>
2022-12-04 17:52:07 -0600

    UI nearly finished

--- projects/project4/index.php
+++ projects/project4/index.php
@@ -10,16 +10,151 @@
     <title>Document</title>
 </head>
 
-<body class="">
-    <div class="container-fluid">
-        <div class="row col-2 h-75 position-absolute top-0">
-            <div class="bg-primary">
-                d
+<body class="container-fluid">
+    <div class="min-vh-100">
+        <div class="row h-75">
+            <div class="row h-75">
+                <div class="row col-3 bg-primary">
+                    <h1>Character Name</h1>
+                    <table class="table table-border text-center m-auto w-50">
+                        <tr>
+                            <th>Health</th>
+                        </tr>
+                        <tr>
+                            <td class="bg-danger">100</td>
+                        </tr>
+                        <tr>
+                            <th>Mana</th>
+                        </tr>
+                        <tr>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <th>Stamina</th>
+                        </tr>
+                        <tr>
+                            <td class="bg-success">100</td>
+                        </tr>
+                    </table>
+                    <h2 class="text-center">Attributes</h2>
+                    <table class="table text-center m-auto w-50">
+                        <tr>
+                            <th>Strength</th>
+                            <th>Perception</th>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <th>Endurance</th>
+                            <th>Charisma</th>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <th>Intelligence</th>
+                            <th>Agility</th>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <th colspan="2">Luck</th>
+                        </tr>
+                        <tr>
+                            <td colspan="2">2</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="col-1">
+
+                </div>
+                <div class="col-8 bg-success">
+                    dialog
+                </div>
             </div>
         </div>
-        <div class="row h-25 w-100 position-absolute bottom-0">
-            <div class="bg-warning">
-                d
+        <div class="row h-25 w-100 bg-warning">
+            <div class="row m-0 p-3">
+                <div class="col-3 p-0">
+                    <h2 class="text-center">Inventory</h2>
+                    <table class="table table-bordered border-light text-center m-auto bg-primary h-100">
+                        <tr class="m-auto">
+                            <td class="bg-light">1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-3 p-0">
+                    <h2 class="text-center">Equipment</h2>
+                    <table class="table table-bordered border-light text-center m-auto bg-primary h-100">
+
+                    </table>
+                </div>
+                <div class="col-1"></div>
+                <div class="col-4 p-0">
+                    <h2 class="text-center">Actions</h2>
+                    <table class="table table-bordered border-light text-center m-auto bg-primary h-100">
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
