acea40187456baad8d58e8ec59900c0a8ea05c55
pgfrank1 <patrickfrank1995@gmail.com>
2022-12-04 19:09:08 -0600

    Added normalize.css for different browsers

--- projects/project4/index.php
+++ projects/project4/index.php
@@ -5,16 +5,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./css/normalize.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
-<body class="container-fluid">
-    <div class="min-vh-100">
-        <div class="row h-75">
-            <div class="row h-75">
-                <div class="row col-3 bg-primary">
+<body style="height: 100% !important;">
+    <div class="container-fluid">
+        <div class="d-flex flex-row justify-content-evenly">
+                <div class="col-3 bg-primary p-4">
                     <h1>Character Name</h1>
                     <table class="table table-border text-center m-auto w-50">
                         <tr>
@@ -36,8 +36,8 @@
                             <td class="bg-success">100</td>
                         </tr>
                     </table>
-                    <h2 class="text-center">Attributes</h2>
-                    <table class="table text-center m-auto w-50">
+                    <h2 class="text-center pt-4">Attributes</h2>
+                    <table class="table text-center m-auto w-50 mb-4">
                         <tr>
                             <th>Strength</th>
                             <th>Perception</th>
@@ -70,17 +70,12 @@
                         </tr>
                     </table>
                 </div>
-                <div class="col-1">
-
-                </div>
-                <div class="col-8 bg-success">
+                <div class="col-9 bg-success float-end">
                     dialog
                 </div>
-            </div>
         </div>
-        <div class="row h-25 w-100 bg-warning">
-            <div class="row m-0 p-3">
-                <div class="col-3 p-0">
+        <div class="d-flex flex-row justify-content-evenly bg-warning" style="max-height: 25% !important;">
+                <div class="col-3 p-4">
                     <h2 class="text-center">Inventory</h2>
                     <table class="table table-bordered border-light text-center m-auto bg-primary h-100">
                         <tr class="m-auto">
@@ -127,15 +122,13 @@
                         </tr>
                     </table>
                 </div>
-                <div class="col-1"></div>
-                <div class="col-3 p-0">
+                <div class="col-3 p-4">
                     <h2 class="text-center">Equipment</h2>
                     <table class="table table-bordered border-light text-center m-auto bg-primary h-100">
 
                     </table>
                 </div>
-                <div class="col-1"></div>
-                <div class="col-4 p-0">
+                <div class="col-3 p-4">
                     <h2 class="text-center">Actions</h2>
                     <table class="table table-bordered border-light text-center m-auto bg-primary h-100">
                         <tr>
@@ -155,13 +148,10 @@
                         </tr>
                     </table>
                 </div>
-            </div>
         </div>
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
 </body>
-
 </html>
