18eb10331c77a2c6b4f51b732544ae1e1f85a9d3
pgfrank1 <patrickfrank1995@gmail.com>
2022-12-07 21:23:06 -0600

    UI currently shows correctly calculated health, mana, stamina and class attributes

--- projects/project4/the-game-functions.php
+++ projects/project4/the-game-functions.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once('dbconnection.php');
 require_once('query-utils.php');
 
@@ -23,4 +24,77 @@ function getAllClassesForPlayer()
     {
         return '<option>NO CLASSES FOUND</option>';
     }
+}
+
+function calculateNewPlayerInformation($newPlayerClass)
+{
+    $newPlayerClass = $newPlayerClass -> fetch_array();
+
+    $_SESSION['player_max_health'] = $newPlayerClass['Endurance'] * 10;
+    $_SESSION['player_max_mana'] = $newPlayerClass['Intelligence'] * 10;
+    $_SESSION['player_max_stamina'] = $newPlayerClass['Strength'] * 10;
+
+    $_SESSION['player_current_health'] = $_SESSION['player_max_health'];
+    $_SESSION['player_current_mana'] = $_SESSION['player_max_mana'];
+    $_SESSION['player_current_stamina'] = $_SESSION['player_max_stamina'];
+
+    $_SESSION['player_strength'] = $newPlayerClass['Strength'];
+    $_SESSION['player_perception'] = $newPlayerClass['Perception'];
+    $_SESSION['player_endurance'] = $newPlayerClass['Endurance'];
+    $_SESSION['player_charisma'] = $newPlayerClass['Charisma'];
+    $_SESSION['player_intelligence'] = $newPlayerClass['Intelligence'];
+    $_SESSION['player_agility'] = $newPlayerClass['Agility'];
+    $_SESSION['player_luck'] = $newPlayerClass['Luck'];
+
+}
+
+function getPlayerStrength()
+{
+    $query = "SELECT Strength FROM Project4.Classes";
+
+    $result_class_strength = mysqli_query(DBC, $query)
+        or trigger_error("There was an issue while querying the database", E_USER_ERROR);
+
+    if (mysqli_num_rows($result_class_strength))
+    {
+        $query = "SELECT Strength FROM Project4.Player_Class";
+
+        $result_player_strength = mysqli_query(DBC, $query)
+            or trigger_error("There was an issue while querying the database", E_USER_ERROR);
+
+        if (mysqli_num_rows($result_player_strength))
+        {
+            return mysqli_fetch_assoc($result_class_strength)['Strength'];
+        }
+    }
+}
+
+function getPlayerPerception()
+{
+
+}
+
+function getPlayerEndurance()
+{
+
+}
+
+function getPlayerCharisma()
+{
+
+}
+
+function getPlayerIntelligence()
+{
+
+}
+
+function getPlayerAgility()
+{
+
+}
+
+function getPlayerLuck()
+{
+
 }
--- projects/project4/the-game.php
+++ projects/project4/the-game.php
@@ -1,6 +1,27 @@
 <?php
     session_start();
-    require_once("page-titles.php");
+    require_once('page-titles.php');
+    require_once('dbconnection.php');
+    require_once('the-game-functions.php');
+    if (empty($_SESSION['player_name']))
+    {
+        header('Location: index.php');
+    }
+
+    if (empty($_SESSION['player_id']) && empty($_SESSION['player_max_health']))
+    {
+        $query = "SELECT * FROM Project4.Classes WHERE ClassId = ?";
+
+        $result = parameterizedQuery(DBC, $query, 'i', $_SESSION['player_class'])
+            or trigger_error("There was an issue while querying the database", E_USER_ERROR);
+
+        calculateNewPlayerInformation($result);
+
+        $_SESSION['player_current_health'] = $_SESSION['player_max_health'];
+        $_SESSION['player_current_mana'] = $_SESSION['player_max_mana'];
+        $_SESSION['player_current_stamina'] = $_SESSION['player_max_stamina'];
+    }
+
     $page_title = HOME_PAGE;
 ?>
 
@@ -21,24 +42,25 @@
         <div class="d-flex flex-row justify-content-evenly p-0" style="min-height: 70vh!important;">
                 <div class="col-4 bg-secondary p-4" style="min-width: 15em !important;">
                     <h1 class="text-center"><?= $_SESSION['player_name']?></h1>
+                    <h2 class="text-center"><?= $_SESSION[] ?></h2>
                     <table class="table table-border text-center m-auto w-75">
                         <tr>
                             <th>Health</th>
                         </tr>
                         <tr>
-                            <td class="bg-danger">100</td>
+                            <td class="bg-danger"><?= $_SESSION['player_current_health']?>/<?= $_SESSION['player_max_health']?></td>
                         </tr>
                         <tr>
                             <th>Mana</th>
                         </tr>
                         <tr>
-                            <td class="bg-primary">100</td>
+                            <td class="bg-primary"><?= $_SESSION['player_current_mana']?>/<?= $_SESSION['player_max_mana']?></td>
                         </tr>
                         <tr>
                             <th>Stamina</th>
                         </tr>
                         <tr>
-                            <td class="bg-success">100</td>
+                            <td class="bg-success"><?= $_SESSION['player_current_stamina']?>/<?= $_SESSION['player_max_stamina']?></td>
                         </tr>
                     </table>
                     <h2 class="text-center pt-4">Attributes</h2>
@@ -48,33 +70,48 @@
                             <th>Perception</th>
                         </tr>
                         <tr>
-                            <td>2</td>
-                            <td>2</td>
+                            <td><?= $_SESSION['player_strength'] ?></td>
+                            <td><?= $_SESSION['player_perception'] ?></td>
                         </tr>
                         <tr>
                             <th>Endurance</th>
                             <th>Charisma</th>
                         </tr>
                         <tr>
-                            <td>2</td>
-                            <td>2</td>
+                            <td><?= $_SESSION['player_endurance'] ?></td>
+                            <td><?= $_SESSION['player_charisma'] ?></td>
                         </tr>
                         <tr>
                             <th>Intelligence</th>
                             <th>Agility</th>
                         </tr>
                         <tr>
-                            <td>2</td>
-                            <td>2</td>
+                            <td><?= $_SESSION['player_intelligence'] ?></td>
+                            <td><?= $_SESSION['player_agility'] ?></td>
                         </tr>
                         <tr>
                             <th colspan="2">Luck</th>
                         </tr>
                         <tr>
-                            <td colspan="2">2</td>
+                            <td colspan="2"><?= $_SESSION['player_luck'] ?></td>
                         </tr>
                     </table>
-                    <a href="login.php" class="d-block btn btn-danger">Login</a>
+                    <a href="save-account.php" class="d-block btn btn-danger">Save Account</a>
+                    <br>
+                    <?php
+                    if (!empty($_SESSION['admin_user_name']))
+                    {
+                    ?>
+                    <a href="admin-page.php" class="d-block btn btn-danger">Go to Admin Page</a>
+                    <?php
+                    }
+                    else
+                    {
+                    ?>
+                    <a href="admin-login.php" class="d-block btn btn-danger">Login to Admin Page</a>
+                    <?php
+                    }
+                    ?>
                 </div>
                 <div class="col-8 bg-success p-4">
                     dialog
