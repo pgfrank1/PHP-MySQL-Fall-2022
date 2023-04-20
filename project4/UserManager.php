4278953c64eb3a13659ad980e28dcc14834a7a16
pgfrank1 <patrickfrank1995@gmail.com>
2023-04-19 21:16:36 -0500

    feat(project4): UserManager now has full PDO methods and uses updated syntax
    Author: pgfrank1

--- project4/UserManager.php
+++ project4/UserManager.php
@@ -2,30 +2,129 @@
 
 class UserManager implements IUserManager
 {
+    private PDO $pdo;
 
-    public function create($username, $password)
+    public function __construct()
     {
-        // TODO: Implement create() method.
+        $this->pdo = $this->db_connect();
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    public function create($username, $password): false|string|null
+    {
+        $return_value= NULL;
+        try {
+            $stmt = $this->pdo->prepare("INSERT INTO User (username, hashed_password, created, api_key) "
+                . "VALUES (:username, :password, CURRENT_TIMESTAMP, :api_key)");
+            $password_hash = $this->hash_password($password);
+            $api_key = $this->generate_api_key();
+
+            $stmt->execute(array(":username"=>$username, ":password"=>$password_hash, ":api_key"=>$api_key));
+            $return_value = $this->pdo->lastInsertId();
+        } catch (Exception $e)
+        {
+            echo "{$e->getMessage()}<br>\n";
+        }
+        return $return_value;
     }
 
-    public function read($id)
+    public function read($id): false|string|null
     {
-        // TODO: Implement read() method.
+        $return_value= NULL;
+        try {
+            $stmt = $this->pdo->prepare("SELECT * FROM User WHERE id = :id");
+            $stmt->execute(array(":id"=>$id));
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $return_value = json_encode($results, JSON_PRETTY_PRINT);
+
+        } catch (Exception $e)
+        {
+            echo "{$e->getMessage()}<br>\n";
+        }
+        return $return_value;
     }
 
-    public function readAll()
+    public function readAll(): false|array
     {
-        // TODO: Implement readAll() method.
+        $return_value= NULL;
+        try {
+            $stmt = $this->pdo->prepare("SELECT * FROM User");
+            $stmt->execute();
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $return_value = json_encode($results);
+
+        } catch (Exception $e)
+        {
+            echo "{$e->getMessage()}<br/>\n";
+        }
+        return $return_value;
     }
 
-    public function update($id, $newPassword)
+    public function updatePassword($id, $newPassword): ?int
     {
-        // TODO: Implement update() method.
+        $return_value= NULL;
+        try {
+            $password_hash = $this->hash_password($newPassword);
+            $stmt = $this->pdo->prepare("UPDATE User SET hashed_password = :password WHERE id = :id");
+            $stmt->execute(array(":password"=>$password_hash));
+            $return_value = $stmt->rowCount();
+        } catch (Exception $e)
+        {
+            echo "{$e->getMessage()}<br/>\n";
+        }
+        return $return_value;
     }
 
-    public function delete($id)
+    public function updateApiKey($id): ?int
     {
-        // TODO: Implement delete() method.
+        $return_value= NULL;
+        try {
+            $new_api_key = $this->generate_api_key();
+            $stmt = $this->pdo->prepare("UPDATE User SET api_key = :api_key WHERE id = :id");
+            $stmt->execute(array(":api_key"=>$new_api_key));
+            $return_value = $stmt->rowCount();
+
+        } catch (Exception $e)
+        {
+            echo "{$e->getMessage()}<br/>\n";
+        }
+        return $return_value;
+    }
+
+    public function delete($id): ?int
+    {
+        $return_value= NULL;
+        try {
+            $stmt = $this->pdo->prepare("DELETE FROM User WHERE id = :id");
+            $stmt->execute(array(":id"=>$id));
+            $return_value = $stmt->rowCount();
+
+        } catch (Exception $e)
+        {
+            echo "{$e->getMessage()}<br/>\n";
+        }
+        return $return_value;
+    }
+
+    public function hash_password($password): string
+    {
+        return password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    public function generate_api_key(): string
+    {
+        $random_number = random_bytes(32);
+        $base62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        $key = "";
+
+        for ($i = 0; $i < 32; $i++)
+        {
+            $value = ord($random_number[$i]);
+            $key .= $base62[$value % 62];
+        }
+
+        return $key;
     }
 
     public function db_connect(): PDO
